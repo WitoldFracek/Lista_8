55e66c4261f23b2853efb601b5bec6dc657456c0
Witold Frącek <witek.fracek@gmail.com>
2022-01-29 14:59:32 +0100

    Initial commit

---
+++ app/src/main/java/com/example/lista_8/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.lista_8
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recordVoiceButton: Button = findViewById(R.id.record_voice_button)
+        recordVoiceButton.setOnClickListener {
+            val intent = Intent(this, VoiceActivity::class.java)
+            startActivity(intent)
+        }
+
+        val recordVideoButton: Button = findViewById(R.id.record_video_button)
+        recordVideoButton.setOnClickListener {
+            val intent = Intent(this, VideoActivity::class.java)
+            startActivity(intent)
+        }
+
+        val playVideoButton: Button = findViewById(R.id.play_video_button)
+        playVideoButton.setOnClickListener {
+            val intent = Intent(this, PlayVideoActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/lista_8/PlayVideoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lista_8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayVideoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_video)
+    }
+}
